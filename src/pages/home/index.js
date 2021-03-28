37ace77b2f18166a332efcb0cce41de1d2541569
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-03-28 17:27:27 -0300

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,153 +6,126 @@ import { RectButton } from 'react-native-gesture-handler';
 
 const Home = () => {
 
-  const navigation = useNavigation();
+    const navigation = useNavigation();
 
-  function handleNavigateToPoints() {
-    navigation.navigate('Points');
-  }
+    function handleNavigateToPoints() {
+        navigation.navigate('Points');
+    }
 
-  function handleNavigateToRegister() {
-    navigation.navigate('Register');
-  }
+    function handleNavigateToRegister() {
+        navigation.navigate('Register');
+    }
 
-  return (
-    <>
-      <ImageBackground  style={styles.container} imageStyle={{ width: 274, height: 368, opacity: .05 }}>
-        <View style={styles.main}>
-          <Image style={styles.img} />
-          <Text style={styles.title}>SteelBack</Text>
-        </View>
+    return (
+        <>
+            <View>
+                <View style={styles.container}>
+                    <View style={styles.menu}>
+                        <Image style={styles.img} />
+                        <Text style={styles.title}>SteelBack</Text>
+                    </View>
+                </View>
+            </View>
 
-        <View style={styles.footer}>
-          <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-            <Text style={styles.buttonText}>
-              ENTRAR
-          </Text>
+        </>
 
-          </RectButton>
-
-          <View style={styles.centerContent}>
-            <Text style={styles.buttonText}>
-              _____ OU _____
-          </Text>
-          </View>
-
-          <RectButton style={styles.button} onPress={handleNavigateToRegister}>
-            <Text style={styles.buttonText}>
-              CADASTRAR
-          </Text>
-
-          </RectButton>
-        </View>
-
-
-      </ImageBackground>
-      <View style={styles.background}>
-        <View style={styles.box}>
-
-        </View>
-      </View>
-
-    </>
-
-  );
+    );
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-    backgroundColor: '#0E0E0E',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#FFF',
+    },
 
-  main: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  box: {
-    backgroundColor: '#28407C',
-    width: '100%',
-    height: 20,
-    borderTopEndRadius: 15,
-    borderTopStartRadius: 15,
-  },
+    menu: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e48040',
+    },
+    box: {
+        backgroundColor: '#28407C',
+        width: '100%',
+        height: 20,
+        borderTopEndRadius: 15,
+        borderTopStartRadius: 15,
+    },
 
-  centerContent: {
-    marginTop: 30,
-    flex: .2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+    centerContent: {
+        marginTop: 30,
+        flex: .2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-  title: {
-    color: '#FFFFFF',
-    fontSize: 32,
-    fontFamily: 'Ubuntu_700Bold',
-    maxWidth: 260,
-    marginTop: 50,
-  },
+    title: {
+        color: '#FFFFFF',
+        fontSize: 32,
+        fontFamily: 'Ubuntu_700Bold',
+        maxWidth: 260,
+        marginTop: 50,
+    },
 
-  description: {
-    color: '#6C6C80',
-    fontSize: 16,
-    marginTop: 16,
-    fontFamily: 'Roboto_400Regular',
-    maxWidth: 260,
-    lineHeight: 24,
-  },
+    description: {
+        color: '#6C6C80',
+        fontSize: 16,
+        marginTop: 16,
+        fontFamily: 'Roboto_400Regular',
+        maxWidth: 260,
+        lineHeight: 24,
+    },
 
-  footer: {
-    flex: 1,
-    alignItems: 'center'
-  },
+    footer: {
+        flex: 1,
+        alignItems: 'center'
+    },
 
-  img: {
-    marginTop: 30,
-  },
-  background:{
-    backgroundColor: '#0E0E0E',
-  },
+    img: {
+        marginTop: 30,
+    },
+    background: {
+        backgroundColor: '#0E0E0E',
+    },
 
-  // input: {
-  //   height: 60,
-  //   backgroundColor: '#FFF',
-  //   borderRadius: 10,
-  //   marginBottom: 8,
-  //   paddingHorizontal: 24,
-  //   fontSize: 16,
-  // },
+    // input: {
+    //   height: 60,
+    //   backgroundColor: '#FFF',
+    //   borderRadius: 10,
+    //   marginBottom: 8,
+    //   paddingHorizontal: 24,
+    //   fontSize: 16,
+    // },
 
-  button: {
-    justifyContent: 'center',
-    backgroundColor: '#28407C',
-    height: 60,
-    width: 250,
-    flexDirection: 'row',
-    borderRadius: 30,
-    overflow: 'hidden',
-    alignItems: 'center',
-    marginTop: 8,
-  },
+    button: {
+        justifyContent: 'center',
+        backgroundColor: '#28407C',
+        height: 60,
+        width: 250,
+        flexDirection: 'row',
+        borderRadius: 30,
+        overflow: 'hidden',
+        alignItems: 'center',
+        marginTop: 8,
+    },
 
-  buttonIcon: {
-    height: 60,
-    width: 60,
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+    buttonIcon: {
+        height: 60,
+        width: 60,
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-  buttonText: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign: 'center',
-    color: '#FFF',
-    fontFamily: 'Roboto_500Medium',
-    fontSize: 16,
-  }
+    buttonText: {
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        color: '#FFF',
+        fontFamily: 'Roboto_500Medium',
+        fontSize: 16,
+    }
 });
 
 export default Home;
