9ca08cf88a146472f3818439e341e4fad0f6b486
Raposo, Claudio Filipe Lima <claudio.f.raposo@accenture.com>
2021-03-28 16:25:03 -0300

    update

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Home from './src'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src';
 
 export default function App() {
   return (
-    <Home />
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
   );
 }
 
