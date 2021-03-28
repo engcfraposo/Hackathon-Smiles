b723048334463b72bbee5f53b276104ecbfc7b5f
Raposo, Claudio Filipe Lima <claudio.f.raposo@accenture.com>
2021-03-28 17:39:17 -0300

    update

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Feather, FontAwesome } from '@expo/vector-icons';
 
 const Menu = () => {
   return (
   <View style={styles.container} >
-    <Text>Componentes</Text>
+    <View style={styles.menu}>
+      
     </View>
+  </View>
     )
 }
 
@@ -13,9 +16,37 @@ export default Menu;
 
 const styles = StyleSheet.create({
   container: {
-    height: 80,
+    width: '100%',
+    paddingTop: 10,
+    paddingHorizontal: 10,
+    height: 90,
     backgroundColor: '#E48040',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
+  menu:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+  },
+  textContainer:{
+    paddingHorizontal: 5,
+  },
+  titleLeftTop:{
+    color: '#fff',
+    textAlign: 'left',
+  },
+  titleLeftBottom:{
+    color: '#fff',
+    textAlign: 'left',
+  },
+  titleRightTop:{
+    color: '#fff',
+    textAlign: 'right',
+  },
+  titleRightBottom:{
+    color: '#fff',
+    textAlign: 'right',
+  }
 });
