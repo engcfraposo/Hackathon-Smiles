692541114ed6a04af9148dc61c68d889f1789d4b
Raposo, Claudio Filipe Lima <claudio.f.raposo@accenture.com>
2021-03-28 21:55:04 -0300

    Update index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -13,9 +13,10 @@ const Home = () => (
   <View style={styles.container}>
     <Menu />
     <ImageBackground source={image} style={styles.image}>
-      <Tooltip popover={<Text>Info here</Text>}>
-        <Image source={miles} style={styles.text} />
-      </Tooltip>
+        <View style={styles.textbox}>
+            <Text style={styles.textMiles}>Olá, me chamo Miles e sou sua Personal Travel. Vamos começar a planejar sua viagem dos sonhos?</Text>
+        </View>
+        <Image source={miles} style={styles.miles} />
     </ImageBackground>
   </View>
 );
@@ -25,12 +26,26 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "column"
   },
+  textbox:{
+    flex: 0.2,
+    marginTop: '70%',
+    marginHorizontal: '20%',
+    backgroundColor: '#FFF',
+    borderBottomLeftRadius: 15,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+  },
+  textMiles: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    color: '#E48040'
+  },
   image: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center"
   },
-  text: {
+  miles: {
     position: 'absolute',
     top: '75%',
     alignSelf: 'flex-end',
